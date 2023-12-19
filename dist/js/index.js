cb0df65b6d5e4b4f2665ed9dc3bb212f51b4db14
rebeckaelin <rebeckaelin@hotmail.com>
2023-12-19 12:15:12 +0100

    updates

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,31 @@
 "use strict";
+let throws = 0, currentGoal = 1;
+const dice = {
+    sides: 6,
+    throw() {
+        return Math.ceil(Math.random() * this.sides);
+    },
+};
+document.querySelector("button").addEventListener("click", () => {
+    let result = dice.throw();
+    document.querySelector("#dice").classList = "";
+    document.querySelector("#dice").classList.add("dice", `dots-${result}`);
+    throws++;
+    document.querySelector("button").innerHTML = `Kasta ( ${throws} )`;
+    console.log(`You threw a ${result}.`);
+    if (result === currentGoal && currentGoal < 6) {
+        console.log("nice, increasing goal!");
+        document.querySelector(`.dots-${result}`).classList.remove("faded");
+        currentGoal++;
+    }
+    else if (result === currentGoal && currentGoal === 6) {
+        console.log(`You rolled a ladder in ${throws} throws.`);
+        document.querySelector(`.dots-${result}`).classList.remove("faded");
+        setTimeout(() => {
+            alert(`You rolled a ladder in ${throws} throws.`);
+            console.log("resetting game....");
+            currentGoal = 1;
+            throws = 0;
+        }, 100);
+    }
+});
